27051f495164596dfa5781d67a0eaecdea047912
Matthew Fan <fn.matthew@outlook.com>
2019-09-05 01:14:53 -0500

    Added background base64 data to use as uri

--- App.js
+++ App.js
@@ -1,28 +1,50 @@
+"use strict";
+
 import React from 'react';
-import { StyleSheet , Text , View , Image } from 'react-native';
+import { StyleSheet , Dimensions , View , Image , ImageBackground } from 'react-native';
+
+import images from './assets/imguri.json';
+
+import ImageEditor from "@react-native-community/image-editor";
+
 
 import ImageButton from "./components/ImageButton.js"
 
+const ITEMS_BACKGROUND = require('./assets/itemsBackground.png');
+const REPEAT_BACKGROUND = require('./assets/repeatingBackground.png');
 const HAMBURGER_ICON = require('./assets/hamburgerMenu.png');
 const GIFT_ICON = require('./assets/gift.png');
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.upperButtonRow}>
+    <ImageBackground
+      source={{uri: images.background}}
+      style={styles.background}
+      imageStyle={styles.backgroundImg}
+    >
+
+      <View style={styles.upperButtonRow}>  
         <ImageButton icon={HAMBURGER_ICON}></ImageButton>
         <ImageButton icon={GIFT_ICON}></ImageButton>
-      </View>
-
-      <Text>Hello from React Native!</Text>
-    </View>
+      </View>      
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  background: {
+    position: 'relative' ,
+    width: Dimensions.get('window').width ,
+    height: Dimensions.get('window').height ,
+    flex: 1 ,
+    // flexDirection: 'row' ,
+    // justifyContent: "center",
+    // alignItems: "center" ,
+  } ,
+  backgroundImg: {
+    position: 'absolute' ,
+    left: 0 ,
+    resizeMode: 'contain' ,
   } ,
   upperButtonRow: {
     flex: 1 ,
