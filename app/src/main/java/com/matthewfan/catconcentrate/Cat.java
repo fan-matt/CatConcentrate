a1596b14cc99a1e3acffcbc94dd657be329c6cff
Matthew Fan <fn.matthew@outlook.com>
2019-09-11 11:26:29 -0500

    Background tick timer and probability based cat spawns

--- app/src/main/java/com/matthewfan/catconcentrate/Cat.java
+++ app/src/main/java/com/matthewfan/catconcentrate/Cat.java
@@ -2,6 +2,8 @@ package com.matthewfan.catconcentrate;
 
 
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Cat {
     private int m_id;
@@ -9,16 +11,21 @@ public class Cat {
     private long m_targetGiftsTime;
     private long m_currentGiftsTime;
     private boolean m_spawned = false;
+    private ImageView m_image;
 
 
-    public Cat(final int ID) {
+    public Cat(final int ID , final double SPAWN_PROBABILITY , final long CURRENT_GIFTS_TIME ,
+               final ImageView IMAGE) {
         m_id = ID;
+        m_spawnProbability = SPAWN_PROBABILITY;
+        m_targetGiftsTime = CURRENT_GIFTS_TIME;
+        m_image = IMAGE;
     }
 
 
     public void tick() {
         if(!m_spawned) {
-            if(Math.random() >= m_spawnProbability) {
+            if(Math.random() <= m_spawnProbability) {
                 m_spawn();
             }
         }
@@ -34,6 +41,7 @@ public class Cat {
         m_spawned = true;
 
         // Make associated view visible
+        m_image.setVisibility(View.VISIBLE);
 
         // Start time record
     }
@@ -43,6 +51,7 @@ public class Cat {
         m_spawned = false;
 
         // Make associated view hidden
+        m_image.setVisibility(View.GONE);
 
         // End time record and add to m_currentGiftsTime and record
     }
--- app/src/main/java/com/matthewfan/catconcentrate/MainActivity.java
+++ app/src/main/java/com/matthewfan/catconcentrate/MainActivity.java
@@ -3,26 +3,89 @@ package com.matthewfan.catconcentrate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.graphics.RectF;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            m_tick();
+            timerHandler.postDelayed(this , 1000);
+        }
+    };
+
+
+    private Cat[] m_cats = new Cat[4];
+    private double[] m_catSpawnProbabilities = {.2 , .5 , .7 , 1};
+    private long[] m_catTargetGiftsTime = {50 , 50 , 50 , 50};
+    private ImageView[] m_catBackgrounds;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        m_catBackgrounds = new ImageView[] {
+            findViewById(R.id.cat1ImageView) ,
+            findViewById(R.id.cat2ImageView) ,
+            findViewById(R.id.cat3ImageView) ,
+            findViewById(R.id.cat4ImageView) ,
+        };
+
+
+        for(int i = 0; i < 4; i ++) {
+            m_cats[i] = new Cat(i , m_catSpawnProbabilities[i] , m_catTargetGiftsTime[i] ,
+                    m_catBackgrounds[i]);
+        }
+
+        for(ImageView imageView : m_catBackgrounds) {
+            imageView.setVisibility(View.GONE);
+        }
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Start ticks
+        timerHandler.post(timerRunnable);
+
+        Log.d("Timer", "resume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        timerHandler.removeCallbacks(timerRunnable);
+
+        // Despawn cats
+        for(Cat cat : m_cats) {
+            cat.despawn();
+        }
+
+        Log.d("Timer", "pause");
+    }
+
 
 
     public void openGiftsActivity(View view) {
         Intent launchGiftsIntent = new Intent(this , GiftsActivity.class);
         startActivity(launchGiftsIntent);
+    }
+
+
+    private void m_tick() {
+        Log.d("Timer", "tick");
+        for(Cat cat : m_cats) {
+            cat.tick();
+        }
     }
 }
